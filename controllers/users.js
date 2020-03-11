38ad766dbf660a3f8e73a94986d313c125744942
Ivan Ivanitskiy <iivanitskiy@mail.ru>
2020-03-11 23:19:51 +0300

    test

--- controllers/users.js
+++ controllers/users.js
@@ -60,7 +60,7 @@ module.exports.login = (req, res, next) => {
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
+          httpOnly: false,
           domain: '',
           // sameSite: true,
         })
